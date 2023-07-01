d22d182c260c6a0133fc3fb88afe72e2e3f733f4
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2023-07-01 00:38:19 -0300

    ajustes do modo claro enquanto navega nas pags

--- Teclado/teclado.js
+++ Teclado/teclado.js
@@ -1,20 +1,30 @@
 // Selecione o link "Modo Escuro" pelo ID
-const darkModeLink = document.getElementById('dark-mode-toggle');           /*para fazer o light mode*/
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const modoTexto = document.getElementById('modo-texto');
+
+// Verifique se há um estado armazenado para o modo claro
+const isLightMode = localStorage.getItem('lightMode') === 'true';
+
+// Defina o estado inicial do modo claro com base no valor armazenado
+if (isLightMode) {
+  document.body.classList.add('light');
+  modoTexto.textContent = 'MODO CLARO';
+} else {
+  document.body.classList.remove('light');
+  modoTexto.textContent = 'MODO ESCURO';
+}
 
 // Adicione um evento de clique ao link
-darkModeLink.addEventListener('click', function() {
+darkModeToggle.addEventListener('click', function() {
   // Alterne a classe 'light' no elemento body
   document.body.classList.toggle('light');
 
-  // Se você também deseja alternar as variáveis do CSS no elemento raiz :root
-  document.documentElement.classList.toggle('light');
-});
+  // Atualize o estado armazenado com base no novo estado do modo claro
+  const isLightMode = document.body.classList.contains('light');
+  localStorage.setItem('lightMode', isLightMode);
 
-const modoTexto = document.getElementById('modo-texto');
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-
-darkModeToggle.addEventListener('click', () => {
-  if (document.body.classList.contains('light')) {                /*para mudar o texto de modo escuro para modo claro */
+  // Alterne o texto do link entre "Modo Escuro" e "Modo Claro"
+  if (isLightMode) {
     modoTexto.textContent = 'MODO CLARO';
   } else {
     modoTexto.textContent = 'MODO ESCURO';
